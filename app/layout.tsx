79ab6cc045bafb1be16791a7f993b390ec8d93cc
gaurav00321 <gauravupadhyay20112003@gmail.com>
2025-07-15 18:44:33 +0530

    minor

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="canonical" href="https://3dteams.com" />
+        <link rel="canonical" href="https://3d-teams.vercel.app" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
